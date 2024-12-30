516274b3be5a90b6b194a6b2411da42e833bc577
Tony Smith <smittytone@icloud.com>
2024-12-30 09:49:56 +0000

    Extend test coverage (contd.)

--- word2textTests/word2textTests.swift
+++ word2textTests/word2textTests.swift
@@ -5,6 +5,16 @@ import Testing
 
 struct word2textTests {
     
+    func buildHeader() -> [UInt8] {
+        // Build header
+        let preamble = "PSIONWPDATAFILE"
+        var bytes: [UInt8] = Array(preamble.utf8)
+        bytes.append(contentsOf: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        return bytes
+    }
+    
+    // MARK: - ProcessText()
+    
     @Test func testProcessTextBadPreamble() async throws {
         
         let bytes: [UInt8] = [UInt8](repeating: 0, count: 100)
@@ -60,14 +70,53 @@ struct word2textTests {
     
     @Test func testProcessTextBadRecordType() async throws {
         
-        let preamble = "PSIONWPDATAFILE"
-        var bytes: [UInt8] = Array(preamble.utf8)
-        bytes.append(contentsOf: [0, 0, 0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0, 0, 0, 0,
-                                  0, 0, 0, 0, 0, 0, 0, 0, 0, 99, 0, 0, 0, 0, 0, 0, 0])
+        var bytes = buildHeader()
+        bytes.append(contentsOf: [0, 99, 0, 0, 0, 0, 0, 0, 0])
         let result: ProcessResult = processFile(bytes[...], "/sample/filepath")
         #expect(result.errorCode == .badRecordType)
     }
     
+    
+    @Test func testProcessTextBadRecordWrongSizeFileInfo() async throws {
+        
+        var bytes = buildHeader()
+        bytes.append(contentsOf: [0, 1, 0, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        let result: ProcessResult = processFile(bytes[...], "/sample/filepath")
+        #expect(result.errorCode == .badRecordLengthFileInfo)
+    }
+    
+    
+    @Test func testProcessTextBadRecordWrongSizePrinterConfig() async throws {
+        
+        // Build record
+        var bytes = buildHeader()
+        bytes.append(contentsOf: [0, 2, 0, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        let result: ProcessResult = processFile(bytes[...], "/sample/filepath")
+        #expect(result.errorCode == .badRecordLengthPrinterConfig)
+    }
+    
+    
+    @Test func testProcessTextBadRecordWrongSizeStyle() async throws {
+        
+        // Build record
+        var bytes = buildHeader()
+        bytes.append(contentsOf: [0, 6, 0, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        let result: ProcessResult = processFile(bytes[...], "/sample/filepath")
+        #expect(result.errorCode == .badRecordLengthStyleDefinition)
+    }
+    
+    
+    @Test func testProcessTextBadRecordWrongSizeEmphasis() async throws {
+        
+        // Build record
+        var bytes = buildHeader()
+        bytes.append(contentsOf: [0, 7, 0, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        let result: ProcessResult = processFile(bytes[...], "/sample/filepath")
+        #expect(result.errorCode == .badRecordLengthStyleDefinition)
+    }
+    
+    
+    // MARK: - GetWord()
     
     @Test func testGetWordValueGoodArray() async throws {
         
@@ -89,6 +138,8 @@ struct word2textTests {
         #expect(getWordValue(bytes[...]) == -1)
     }
     
+    
+    // MARK: - GetOuterText()
     
     @Test func testGetOuterTextGoodHeaderString() async throws {
         
@@ -124,6 +175,8 @@ struct word2textTests {
     }
     
     
+    // MARK: - GetBodyText()
+    
     @Test func testGetBodyTextGoodSubstitutions() async throws {
         
         let body = "fintlewoodlewix\042"
@@ -135,6 +188,8 @@ struct word2textTests {
         #expect(backBytes[15] == 0x0A && backBytes[18] == 0x2D)
     }
     
+    
+    // MARK: - GetFullPath()
     
     @Test func testGetFullPathGoodDots() async throws {
         
@@ -152,6 +207,8 @@ struct word2textTests {
     }
     
     
+    // MARK: - ProcessRelativePath()
+    
     @Test func testProcessRelativePath() async throws {
         
         let path = "/example/test/folder/../../../"
@@ -159,4 +216,55 @@ struct word2textTests {
         #expect(aPath == "/tmp")
     }
     
+    
+    // MARK: - DoesPathReferenceDirectory()
+    
+    @Test func textDoesPathReferenceDirectoryWithDir() async throws {
+        
+        let path = "/"
+        #expect(doesPathReferenceDirectory(path))
+    }
+    
+    
+    @Test func textDoesPathReferenceDirectoryWithFile() async throws {
+        
+        let path = "/tmp/test"
+        do {
+            try "test".write(toFile: path, atomically: true, encoding: .utf8)
+            #expect(!doesPathReferenceDirectory(path))
+        } catch {
+            #expect(Bool(false))
+        }
+    }
+    
+    
+    // MARK: - GetFileContents()
+    
+    @Test func textDoesGetFileContentsValidFile() async throws {
+        
+        let path = "/tmp/test"
+        do {
+            try "test".write(toFile: path, atomically: true, encoding: .utf8)
+            let readback = getFileContents(path)
+            #expect(readback.count == 4)
+        } catch {
+            #expect(Bool(false))
+        }
+    }
+    
+    
+    @Test func textDoesGetFileContentsNonexistentFile() async throws {
+        
+        let path = "/tmp/testz"
+        let readback = getFileContents(path)
+        #expect(readback.isEmpty)
+    }
+    
+    
+    @Test func textDoesGetFileContentsDir() async throws {
+        
+        let path = "/tmp"
+        let readback = getFileContents(path)
+        #expect(readback.isEmpty)
+    }
 }
